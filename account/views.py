5ce0019cf1dd9d979e2993a973dac43673b20645
Msa2001 <msa2001b21gmail.com>
2022-11-19 18:19:30 +0330

    Login

--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegisterationForm
+from django.contrib.auth import login, authenticate
+from .forms import UserRegisterationForm, UserLoginForm
 from django.contrib import messages
 
 # Create your views here.
@@ -20,3 +21,24 @@ class UserRegisterView(View):
             messages.success(request, 'You registered successfully', 'success')
             return redirect('home:home')
         return render(request, 'account/register.html', {'form': form})
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'account/login.html'
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You have logged in successfully')
+                return redirect('home:home')
+            messages.error(request, 'Username or password are not correct', 'danger')
+            return render(request, self.template_name, {'form': form})
