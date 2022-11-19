782fa2d44fe87b0d75f0fc9fd0275515a68c96b1
Msa2001 <msa2001b21gmail.com>
2022-11-19 16:38:42 +0330

    Register form and errors

---
+++ account/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserRegisterationForm
+from django.contrib import messages
+
+# Create your views here.
+
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserRegisterationForm()
+        return render(request, 'account/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserRegisterationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, 'You registered successfully', 'success')
+            return redirect('home:home')
+        return render(request, 'account/register.html', {'form': form})
