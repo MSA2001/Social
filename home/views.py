6a6ab9d93253e7d1fa0d41b278db90f1bee3a41c
Msa2001 <msa2001b21gmail.com>
2022-11-23 19:46:06 +0330

    Post Delete View

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 # Create your views here.
 
 
@@ -16,4 +18,18 @@ class PostDetailView(View):
     def get(self, request, post_id, post_slug):
         post = Post.objects.get(id=post_id, slug=post_slug)
         return render(request, 'home/detail.html', {'post': post})
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request, 'Post deleted successfully', 'success')
+        else:
+            messages.error(request, 'you can`t delete this post', 'danger')
+
+        return redirect('home:home')
+
+
 
