a7b11a9821f219cf7c3bdd95f3969ea43c42de52
Msa2001 <msa2001b21gmail.com>
2022-11-29 18:02:58 +0330

    password reset confirm view

--- account/urls.py
+++ account/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 app_name = 'account'
 urlpatterns = [
-    path('register', views.UserRegisterView.as_view(), name='register'),
-    path('login', views.UserLoginView.as_view(), name='login'),
-    path('logout', views.UserLogoutView.as_view(), name='logout'),
-    path('profile/<int:user_id>', views.UserProfileView.as_view(), name='profile'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('profile/<int:user_id>/', views.UserProfileView.as_view(), name='profile'),
     path('reset/', views.UserPasswordResetView.as_view(), name='reset_password'),
-    path('reset/done', views.UserPasswordResetDoneView.as_view(), name='password_reset_done')
+    path('reset/done', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('confirm/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm')
 ]
