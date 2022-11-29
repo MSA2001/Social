89c566919ab99b7b0679720f988bda9a2da0b2d0
MSA2001 <msa2001b21@gmail.com>
2022-11-29 17:39:18 +0330

    password reset done view

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login', views.UserLoginView.as_view(), name='login'),
     path('logout', views.UserLogoutView.as_view(), name='logout'),
     path('profile/<int:user_id>', views.UserProfileView.as_view(), name='profile'),
-    path('reset/', views.UserPasswordResetView.as_view(), name='reset_password')
+    path('reset/', views.UserPasswordResetView.as_view(), name='reset_password'),
+    path('reset/done', views.UserPasswordResetDoneView.as_view(), name='password_reset_done')
 ]
