db25ffefbe520daef051a334e58d2756fd871ee5
Msa2001 <msa2001b21gmail.com>
2022-12-06 23:53:19 +0330

    Create new comment form added

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 
 class PostUpdateForm(forms.ModelForm):
@@ -12,3 +12,12 @@ class PostCreateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'body')
+
+
+class CommentCreateForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+        widgets = {
+            'body': forms.Textarea(attrs={'class': 'form-control'})
+        }
