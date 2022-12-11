b96b2c6dea6d5ac1e26b0661954f108d9d0a7b3a
MSA2001 <msa2001b21@gmail.com>
2022-12-12 00:00:51 +0330

    Edit profile added

--- account/views.py
+++ account/views.py
@@ -2,13 +2,13 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.views import View
 from django.contrib.auth import login, authenticate, logout
-from .forms import UserRegisterationForm, UserLoginForm
+from .forms import UserRegisterationForm, UserLoginForm, UserEditForm
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from home.models import Post
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
-from .models import Relation
+from .models import Relation, Profile
 
 # Create your views here.
 
@@ -125,3 +125,21 @@ class UserUnfollowView(LoginRequiredMixin, View):
         else:
             messages.error(request, 'You are not following this user', 'danger')
         return redirect('account:profile', user_id)
+
+
+class EditUserView(LoginRequiredMixin, View):
+    form_class = UserEditForm
+
+    def get(self, request):
+        form = self.form_class(instance=request.user.profile, initial={'email': request.user.email})
+        return render(request, 'account/edit_profile.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            request.user.email = form.cleaned_data['email']
+            request.user.save()
+            messages.success(request, 'You edited your profile successfully', 'success')
+        return redirect('account:profile', request.user.id)
+
